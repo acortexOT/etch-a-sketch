5091c5aac23f8a24fe38d5f4ca47e18499034d7d
acortexot <acortexot@gmail.com>
2022-08-07 20:46:37 -0300

    Add algorhythm to scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//Loop createElement for 16*16 div with class 'item [i]', (get width and safe as max)
+//hover eventListener triggers function 
+    //if no color: that randomly assigns RBG background color to item
+    //if color: increases darkness by 10%
+//button triggers NewGrid function
+    //prompt asks user to input number of squares per side for new grid
+    //input is checked for being number less than 101
+        //if true: deletes all divs from container and loops divs with entered number
+        //if number but less than 100, cheeky prompt to retry with smaller number, between 10-100
+        //elseif not a number: than prompt to retry using a number between 10 - 100
+        //else empty return
