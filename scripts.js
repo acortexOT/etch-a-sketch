fd713a591ef39f50d6246079262c38cd7d6153b9
acortexot <acortexot@gmail.com>
2022-08-08 18:13:26 -0300

    Finish newCanvas function. Add black mouseover event

--- scripts.js
+++ scripts.js
@@ -1,14 +1,24 @@
-newCanvas(16);
-//Loop createElement for canvas with divs class grid-item and id 'item [i]', (get width and safe as max)
+newCanvas(16);  //load page with 16x16 grid
+//Loop createElement for divs of canvas for i number of iterations inputted by user
 function newCanvas(number) {
+const gridUnit = Math.floor(1/(number));
 const container = document.querySelector('#container');
+container.style.gridTemplateColumns = `repeat(${number}, auto)`;
 for (let i = 1; i < number**2 + 1; ++i) {
     let activeGrid = document.createElement('div');
     activeGrid.classList.add('grid-item')
     activeGrid.id = `item-${i}`;
+    activeGrid.textContent = `${i}`;
     container.appendChild(activeGrid);
 }};
 //hover eventListener triggers function 
+const gridItem = document.querySelectorAll('.grid-item'); //select all divs inside canvas
+gridItem.forEach((gridDiv) => {                           
+    gridDiv.addEventListener('mouseover', () => {   //activate selected div once mouse hovers over it
+        gridDiv.style.backgroundColor = `black`;
+    });
+});
+
     //if no color: that randomly assigns RBG background color to item
     //if color: increases darkness by 10%
 //button triggers NewGrid function
