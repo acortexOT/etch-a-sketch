9370127783b2b5c57a8b6db6f82d9f1a5f465663
acortexot <acortexot@gmail.com>
2022-08-09 19:56:00 -0300

    Finish work-around for darkening colors

--- scripts.js
+++ scripts.js
@@ -1,36 +1,30 @@
 makeNewCanvas(16);  //load page with 16x16 grid
-//Loop createElement for divs of canvas for i number of iterations inputted by user
+makeColorChange();  //load event listener
 
-function makeNewCanvas(number) {
-const gridUnit = Math.floor(1/(number));
-const container = document.querySelector('#container');
-while(container.firstChild) {   //remove old canvas if present
-    container.removeChild(container.firstChild);
-};
-container.style.gridTemplateColumns = `repeat(${number}, auto)`; //set up grid template
-    for (let i = 1; i < number**2 + 1; ++i) {
-        let activeGrid = document.createElement('div');
-        activeGrid.classList.add('grid-item')
-        activeGrid.id = `item-${i}`;
-        container.appendChild(activeGrid);
-        activeGrid.addEventListener('mouseover', () => {   //activate selected div once mouse hovers over it
-            if (activeGrid.style.backgroundColor === "") {  //if no color randomly select color
-                activeGrid.style.backgroundColor = `${getRandomColor()}`;  
-            }else { //if color, darken color by 10%
-                
-
-                for (i = 0; i < 101; i+=10) {
-                    if (activeGrid.style.filter === `brightness(${i}%)`) {   //THIS IS PROBABLY THE AREA THAT ISN'T WORKING!
-                        activeGrid.style.filter = `brightness(${i-10}%)`;
-                    }
-                }
-
-
-            }
-        });
+function makeNewCanvas(number) {    //Loop createElement for divs of canvas for i number of iterations inputted by user
+    const gridUnit = Math.floor(1/(number));
+    const container = document.querySelector('#container');
+    while(container.firstChild) {   //remove old canvas if present
+        container.removeChild(container.firstChild);
     };
+    container.style.gridTemplateColumns = `repeat(${number}, auto)`; //set up grid template
+        for (let i = 1; i < number**2 + 1; ++i) {
+            let activeGrid = document.createElement('div');
+            activeGrid.classList.add('grid-item')
+            activeGrid.id = `item-${i}`;
+            container.appendChild(activeGrid);
+        };
+    };
+function makeColorChange() {   
+    const gridItem = document.querySelectorAll('.grid-item');
+    gridItem.forEach((item) =>{
+        item.addEventListener('mouseover', () => {   //activate selected div once mouse hovers over it
+            if (item.style.backgroundColor === "") {  //if no color randomly select color
+                item.style.backgroundColor = `${getRandomColor()}`;  
+            }else {item.style.backgroundColor = `${getDarkerColor(item.style.backgroundColor)}`;
+        }});
+        });
 };
-
     //if no color: that randomly assigns RBG background color to item
     //if color: increases darkness by 10%
 
@@ -45,8 +39,8 @@ function getCanvasSize() {
         userInput = prompt('Input out of range. You must choose a number between 10 and 100.', '16');
         num = parseInt(userInput);
     };
-    return makeNewCanvas(num);
-    
+    makeNewCanvas(num);
+    makeColorChange();
 }
 
 function getRandomColor() {
@@ -54,4 +48,16 @@ function getRandomColor() {
     let g = Math.floor(Math.random()*255);
     let b = Math.floor(Math.random()*255);
     return 'rgb('+ r +',' + g +','+ b + ')';
+}
+
+function getDarkerColor(rgbColor) {
+    rgbColor = rgbColor.slice(4,-1);    //remove rgb( and )
+    let arrColor = rgbColor.split(",");     //convert to array of 3 numbers
+
+    let r = Math.floor(arrColor[0]*.7); //decrease by 30% and round to lowest whole number
+    let g = Math.floor(arrColor[1]*.7);
+    let b = Math.floor(arrColor[2]*.7);
+
+    const darkColor = 'rgb('+ r +',' + g +','+ b + ')'  //concat to build new rgb color
+return darkColor; 
 }
