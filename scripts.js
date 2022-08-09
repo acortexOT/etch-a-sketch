99c7714187ffdc24f3aaf54624027ba69a94a301
acortexot <acortexot@gmail.com>
2022-08-09 07:08:12 -0300

    Add style to button and container. Update fonts and alignment

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-newCanvas(16);  //load page with 16x16 grid
+makeNewCanvas(16);  //load page with 16x16 grid
 //Loop createElement for divs of canvas for i number of iterations inputted by user
 
-function newCanvas(number) {
+function makeNewCanvas(number) {
 const gridUnit = Math.floor(1/(number));
 const container = document.querySelector('#container');
 while(container.firstChild) {   //remove old canvas if present
@@ -21,21 +21,22 @@ for (let i = 1; i < number**2 + 1; ++i) {
 
     //if no color: that randomly assigns RBG background color to item
     //if color: increases darkness by 10%
-//button triggers NewGrid function
-const button = document.querySelector('#button');
+
+const button = document.querySelector('#button');   //button triggers function to make new canvas
 button.addEventListener('click', getCanvasSize);
 
 function getCanvasSize() {
     let userInput = prompt('Please enter a number between 10 and 100 for your canvas size:', '16');
     let num = parseInt(userInput);
-    return newCanvas(num);
+    if (!userInput) return; //stop code if window is closed.
+    while (!(num > 10 && num < 101)) {  //check for appropriate number range. return to prompt window if incorrect
+        userInput = prompt('Input out of range. You must choose a number between 10 and 100.', '16');
+        num = parseInt(userInput);
+    };
+    return makeNewCanvas(num);
+    
 }
-    //prompt asks user to input number of squares per side for new grid
-    //input is checked for being number less than 101
-        //if true: deletes all divs from container and loops divs with entered number
-        //if number but less than 100, cheeky prompt to retry with smaller number, between 10-100
-        //elseif not a number: than prompt to retry using a number between 10 - 100
-        //else empty return
+
 function getRandomColor() {
     let r = Math.floor(Math.random()*255);
     let g = Math.floor(Math.random()*255);
